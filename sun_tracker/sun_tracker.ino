229542c67106ced00a5d5ea6a63d02c1edbc351e
Rick Holmes <rickh877@gmail.com>
2020-04-05 15:13:51 -0400

    initial test software

---
+++ sun_tracker/sun_tracker.ino
@@ -0,0 +1,36 @@
+int westLED = 2;
+int eastLED = 3;
+
+int westSensor = A1;
+int eastSensor = A0;
+int westValue = 0;
+int eastValue = 0;
+
+void setup() {
+  // solar panel sun tracker
+  // set-up code. Analog pins do not need to be set up
+  Serial.begin(9600);
+  pinMode (westLED, OUTPUT);
+  pinMode (eastLED, OUTPUT);
+  digitalWrite (westLED, LOW);
+  digitalWrite (eastLED, LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  westValue = analogRead(westSensor);
+  eastValue = analogRead(eastSensor);
+  Serial.print("westValue is: ");
+  Serial.print(westValue, DEC);
+  Serial.print("  eastValue is: ");
+  Serial.println(eastValue, DEC);
+  if (westValue > 50){
+        digitalWrite(westLED, HIGH);
+  }
+  if (eastValue > 50){
+        digitalWrite(eastLED, HIGH);
+  }
+  delay(200);
+  digitalWrite(westLED, LOW);
+  digitalWrite(eastLED, LOW);
+}
